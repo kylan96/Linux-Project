1db3c30d47fc06350d0374375c304c3419654aa5
Kylan Roberts <kylantiger@gmail.com>
2020-12-29 17:41:45 -0500

    Pizza Executable update

--- pizza.sh
+++ pizza.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
+clear
+# Creators: Kylan Roberts, Joe Mitchell, Rachid, Byron Davis
 
-#----------- Establishing prices for all of our Pizza Sizes and Toppings ----------------
+#--------- List of Meat Toppings ------------
+Pepperoni=1.00
+#Sausage
+#Beef
+#Ham
+#Bacon 
+#Salami
+#--------- List of Veggie Toppings ----------
+Onions=1.00
+#Tomatoes
+#Banana Peppers
+#Green Peppers
+#Jalapeno Peppers
+#Black Olives
+#Mushrooms
+#--------- List of Cheese Toppings ----------
+#Cheddar
+#Mozerella
+#Feta Cheese
+#--------- List of Pizza Sauce Options --------
+#--------- List of Crust Options ------------
+#Thinned
+#Stuffed
+#Pan Tossed
+#--------- Carryout or Delivery Prices -------
+#Carryout
+#Delivery
+#----------- Establishing prices for all of our Pizza Sizes ----------------
+#small
+#medium
+#large
+#big
 
-#meat=(Pepporini Bacon Sausage Ham ) ( $4.99 each)
-#veggies= ( Black Olives, Green Peppers, Mushrooms, Spinach, Onions, )
-#cheeses= ( Cheese or Extra Cheese ) ( Extra Cheese will be an additional $2.00)
-#crust= ( Thin Crust, Stuffed Crust, Garlic Hand Tossed, Hand Tossed ) (Extra $1.50 for Stuffed Crust)
 
 
 #---------- Intro --------------
@@ -26,11 +55,34 @@ else
 echo "No problem, let's start off with size. What size pizza would you like?"
 read -p "Please choose small, medium, large, or big. >> " pizzasize
 fi
-
 if [[ $pizzasize == small ]] ; then
-echo "Alright so that is a small pizza. What would you like on it $name?" 
-echo "Please choose a meat first."
-#List toppings here
+#--------- Remember to create another if statement for if they choose no ----------------
+echo "Alright so that is a small pizza. What would you like on it $name?"
+read -p "Would you like a meat topping? " answer
+if [[ $answer == yes ]] ; then
+counter=1
+meatoptions=(Pepperoni Sausage Beef Ham Bacon Salami)
+echo "Here is the list of meat options for your pizza."
+echo "----------------"
+for t in ${meatoptions[@]}
+do
+echo "$counter. $t"
+((counter++))
+done
+echo "---------"
+echo""
+read -p "Please choose a number 1-6 >> " selection
+if [ $selection -eq 1 ] ; then
+echo "Great you have chosen ${meatoptions[0]}. Nice!"
+fi
+else 
+echo  "Alright $name, on to veggies."
+fi
+
+
+# Choosing a vegetable option---------------
+#veggietoppings=( 
+#-----End of the first small if loop ---------------
 
 elif [[ $pizzasize == medium ]] ; then
 echo "Alright so that is a medium pizza. What would you like on it $name?" 
@@ -48,13 +100,6 @@ else
 echo "Please enter a valid choice." 
 fi
 
-#-------------- Showing Toppings here ----------------------
-echo "Which meat topping(s) would you like $name?"
 
-
-
-
-
-
-#------- Creating Arrays for Toppings, Pizza Sizes------------
+#------- Choosing Carryout or Delivery ------------
 
